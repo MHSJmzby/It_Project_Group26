47a8b26a3733f4f82ba3d1f64bf895997e710c29
Ning535 <122553451+Ning535@users.noreply.github.com>
2023-03-22 20:30:42 +0000

    Update UserCenter.vue

--- it26/src/views/Userviews/UserCenter.vue
+++ it26/src/views/Userviews/UserCenter.vue
@@ -1,132 +1,95 @@
 <template>
   <div style="width: 100%; padding: 10px; position: center">
-    <!--  Search  -->
-    <div>
-      <el-input v-model="search" placeholder="Please input Movie name"  style="width: 300px" clearable />
-      <el-button style="margin: 10px" plain @click="load">Search</el-button>
+<!--    Personal Information-->
+    <div style="margin: 10px" class="demo-basic--circle">
+      <div class="block">
+        <el-avatar :size="100" :src="'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'" />
+      </div>
     </div>
-
-    <!--    Table   -->
-    <div class="block">
-      <el-date-picker
-          v-model="timing"
-          type="date"
-          placeholder="Pick a day"
-      />
-      <el-button style="margin: 10px" plain @click="load">Check</el-button>
-    </div>
-    <el-table :data="tableData" style="width: 100%; "  default-expand-all>
-      <el-table-column type="index"  align="center"/>
-      <el-table-column type="expand" style="width: 200px;" header-align="center" >
-        <template #default="props">
-          <div m="4" style="padding: 10px;">
-            <h3>Description</h3>
-            <p m="t-0 b-2">Movie: {{ props.row.name }}</p>
-            <p m="t-0 b-2">Actor: {{ props.row.actor }}</p>
-            <p m="t-0 b-2">Introduction: {{ props.row.introduction }}</p>
-          </div>
-        </template>
-      </el-table-column>
-
-      <el-table-column label="Movie" prop="name"  align="center" sortable/>
-      <el-table-column label="Date" prop="releaseTime" align="center" sortable/>
-      <el-table-column label="Position" prop="screen" align="center"/>
-      <el-table-column label="Price" prop="price" align="center"/>
-      <el-table-column label="Operations" align="center">
-        <template #default="scope">
-          <el-button size="small" type="primary" @click="book(scope.row.id)">Book</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-
-    <div style="margin: 10px 0; display: flex">
-      <!--      Dialog   -->
-      <el-dialog
-          v-model="Dialogbook"
-          title="Tips"
-          width="30%"
-          align-center
-      >
-        <span>Do you confirm the booking?</span>
-        <template #footer>
-        <span class="dialog-footer">
-          <el-button type="primary" @click="confirm">
-            Confirm
-          </el-button>
-          <el-button @click="Dialogbook = false">Cancel</el-button>
-        </span>
-        </template>
-      </el-dialog>
-    </div>
-
+    <el-card style="width: 40%; ">
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="ID" prop="id">
+          <el-input v-model="form.id" disabled></el-input>
+        </el-form-item>
+        <el-form-item label="Username" prop="name">
+          <el-input v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item label="Email" prop="email">
+          <el-input v-model="form.email"></el-input>
+        </el-form-item>
+        <el-form-item label="Password" prop="password">
+          <el-input v-model="form.password" show-password></el-input>
+        </el-form-item>
+      </el-form>
+      <div style="text-align: center">
+        <el-button type="primary" @click="Dialoguser=true">Save</el-button>
+      </div>
+    </el-card>
   </div>
 
+  <div>
+    <el-dialog
+        v-model="Dialoguser"
+        title="Tips"
+        width="30%"
+        align-center
+    >
+      <span>Do you confirm the Edit?</span>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button type="primary" @click="update">
+            Confirm
+          </el-button>
+          <el-button @click="Dialoguser = false">Cancel</el-button>
+        </span>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 import request from "@/utils/request";
 
 export default {
-  name: "Booking",
+  name: "UserCenter",
   components: {
 
   },
   data() {
     return {
-      search: '',
-      timing: '',
-      Dialogbook: false,
-      tableData:  [],
-      form: {}
+      Dialoguser: false,
+      form: {},
+
     }
 
 
   },
   created() {
-    this.load()
+    let str = sessionStorage.getItem("user") || "{}"
+    console.log(JSON.parse(JSON.parse(str))[0]["fields"])
+    let temp=JSON.parse(JSON.parse(str))[0]["fields"]
+    temp["id"]=JSON.parse(JSON.parse(str))[0]["pk"]
+    this.form = temp
   },
-
   methods: {
-    load() {
-      request.get("/booking/",{
-        params: {
-          search: this.search,
-          timing: this.timing
-        }
-      }).then(res => {
+    update() {
+      request.put("/usercenter/", this.form).then(res => {
         console.log(res)
-        this.tableData = res.data
-      })
-    },
-    book(id)
-    {
-      this.Dialogbook = true
-      const userJson = sessionStorage.getItem("user") || "{}";
-      let user=JSON.parse(JSON.parse(userJson))[0]["fields"]
-      user["id"]=JSON.parse(JSON.parse(userJson))[0]["pk"]
-      this.form.userid = JSON.parse(JSON.stringify(user.id ));
-      this.form.movieid = JSON.parse(JSON.stringify(id))
-      console.log(this.form);
-    },
-    confirm()
-    {
-      console.log(this.form)
-      request.post("/booking/", this.form).then(res => {
-        console.log(res)
-        if (res.code ==='0') {
+        if (res.code === '0') {
           this.$message({
-            type:"success",
-            message: "Booking Successfully!"
+            type: "success",
+            message: "Update Successfully"
+          })
+          sessionStorage.setItem("user", JSON.stringify(this.form))
+          // 触发Layout更新用户信息
+          this.$emit("userInfo")
+        } else {
+          this.$message({
+            type: "error",
+            message: res.msg
           })
         }
-        else {
-          this.$message( {
-            type:"error",
-            message: res.msg,
-          })
-        }
-        this.load()   //刷新表格内容
-        this.Dialogbook = false
+        this.Dialoguser = false
       })
     }
 
